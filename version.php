702a06efff8ef0817a9a4c55cc973b07260eb535
Ponlawat W. <ponlawat_w@outlook.co.th>
2025-09-28 13:24:07 +0700

    Bumped version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_forumownpostfilter';
-$plugin->release = '0.0.1';
-$plugin->version = 2025041400;
+$plugin->release = '1.0.0';
+$plugin->version = 2025092800;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
